7b20daa6ac054a8a0b92e81715b55231baff7118
Mehdi Mohammdi <maxmehti2@gmail.com>
2025-09-14 01:37:25 +0330

    feat: last parts of changes in 404 page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="main">
     <div class="content">
+      <div class="rotate_section" :style="rotateMain"></div>
       <span class="title_content">404</span>
+      <span class="title_content-change" :style="rotateStyleChange">404</span>
       <span class="main_text">صفحه مورد نظر یافت نشد</span>
       <div class="btn_content">
         <span class="text_btn">بازگشت به صفحه اصلی</span>
@@ -11,6 +13,44 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
+
+const rotateMain = ref({
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+});
+
+const rotateStyleChange = ref({
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+});
+
+onMounted(() => {
+  setInterval(() => {
+    const offsetTop = 45 + Math.sin(Date.now() / 750) * 5;
+    const offsetLeft = 50 + Math.cos(Date.now() / 750) * 6;
+    const translateX = -offsetLeft;
+    const translateY = -offsetTop;
+
+    const offsetTopChange = 30 + Math.sin(Date.now() / 750) * 5;
+    const offsetLeftChange = 45 + Math.cos(Date.now() / 750) * 5;
+    const translateXChange = -offsetLeftChange;
+    const translateYChange = -offsetTopChange;
+
+    rotateMain.value = {
+      top: offsetTop + "%",
+      left: offsetLeft + "%",
+      transform: `translate(${translateX}%, ${translateY}%)`,
+    };
+    rotateStyleChange.value = {
+      top: offsetTopChange + "%",
+      left: offsetLeftChange + "%",
+      transform: `translate(${translateXChange}%, ${translateYChange}%)`,
+    };
+  }, 16);
+});
 </script>
 
 <style>
@@ -27,15 +67,18 @@ body {
 }
 
 .main {
+  position: relative;
+  overflow: hidden;
   display: flex;
   height: 100vh;
   justify-content: center;
-  background-color: black;
+  background-color: #737373;
 }
 
 .btn_content {
   background: #22ce83;
   color: white;
+  z-index: 111111111;
   font-size: 1.2rem;
   cursor: pointer;
   display: flex;
@@ -58,13 +101,33 @@ body {
   margin-top: 1rem;
   margin-bottom: 1rem;
   font-weight: 400;
+  z-index: 111111111;
   color: white;
+}
+
+.rotate_section {
+  position: absolute;
+  background: radial-gradient(40% 40% at 50% 42%, white, #000 35%) !important;
+  z-index: 11;
+  height: 250vmax;
+  width: 250vmax;
+  box-shadow: rgba(245, 243, 243, 0.56) 0px 22px 70px 4px;
 }
 
 .title_content {
   text-align: center;
   font-size: 18rem;
   line-height: 18rem;
-  color: #838996;
+  color: #737373;
+  z-index: 1111111;
+}
+.title_content-change {
+  text-align: center;
+  position: absolute;
+  font-size: 18rem;
+  line-height: 18rem;
+  color: black;
+  filter: blur(1.5vmin);
+  z-index: 111111;
 }
 </style>
